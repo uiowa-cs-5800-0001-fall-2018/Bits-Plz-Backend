f8ca4863094a77ea7e1ee023ac92a127f4acfcd0
Rentian Dong <r2dong@yahoo.com>
2018-11-11 14:40:22 -0600

    hacked sentiment analysis to work

--- app/routes/twitter_search_routes.js
+++ app/routes/twitter_search_routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app, db) {
-	var twitter_key;
+	let twitter_key;
 	if (app.settings.env == 'development') {
 		twitter_key = require('../key.js');
 	} 
@@ -11,14 +11,50 @@ module.exports = function(app, db) {
 			access_token_secret: process.env.access_token_secret
 		}
 	}
-	var Twitter = require('twitter');
-	var client = new Twitter(twitter_key);
+	let Twitter = require('twitter');
+	let client = new Twitter(twitter_key);
+	let Sentiment = require('sentiment');
+    let sentiment = new Sentiment();
+	let async = require('async');
 
-	app.post('/search', (req, res) => {
+    app.post('/search', (req, res) => {
 		client.get('search/tweets', {q: 'trump'}, function(error, tweets, response) {
-   			console.log(tweets);
-			res.send(tweets);
+   			let parsed_tweets = [];
+   			async.forEach(
+   				tweets.statuses,
+				(tweet, call_back) => {
+					let hash_tags = [];
+					for (var hashtag_json in tweet.entities.hashtags)
+						hash_tags.push(hashtag_json.text);
+					let tweet_simplified = {
+						titles: hash_tags,
+						region: tweet.user.location === '' ? null : tweet.user.location,
+						time: tweet.created_at,
+						content: tweet.text,
+						score: sentiment.analyze(tweet.text).score
+					};
+					parsed_tweets.push(tweet_simplified);
+					call_back(null);
+				},
+				(err) => {
+					if (err) {
+						console.log("an error occurred: ", err);
+						res.send('an internal server error occurred');
+					}
+					else {
+						console.log("processsing finished without any error");
+						parsed_tweets.sort(function (tweet_1, tweet_2) {
+							if (tweet_1.score < tweet_2.score)
+								return -1;
+							else if (tweet_1.score > tweet_2.score)
+								return 1;
+							else
+								return 0;
+						});
+						res.send(parsed_tweets);
+					}
+				}
+			);
 		});
-		console.log(req.body);
 	});
 };
