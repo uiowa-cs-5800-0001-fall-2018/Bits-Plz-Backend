034519bec9eab38a4cef1053c99c5185d532f24e
Rentian Dong <r2dong@yahoo.com>
2018-11-21 11:53:00 -0600

    mailer working

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
-var cors = require('cors')
+const cors = require('cors');
+const CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'yahoo',
+    auth: {
+        user: 'bitz.please@yahoo.com',
+        pass: 'alekseygurtovoy'
+    }
+});
 
 app.use(
 	bodyParser.urlencoded({extended: true}),
@@ -14,10 +24,24 @@ app.listen(port, () => {
     console.log('We are live on ' + port);
 	console.log('Environment: ' + app.settings.env);
 
-	var CronJob = require('cron').CronJob;
+
 	new CronJob('0 * * * * *', () => {
   		console.log('message');
 	}, null, true, 'America/Chicago').start();
+
+	var mailOptions = {
+    	from: 'bitz.please@yahoo.com',
+    	to: 'r2dong@yahoo.com',
+    	subject: 'Sending Email using Node.js',
+    	text: 'That was easy!'
+	};
+
+	transporter.sendMail(mailOptions, function(error,info){
+    	if(error) {
+        	console.log(error);
+    	}
+    	else {
+        	console.log('Email sent: ' + info.response);
+    	}
+	});	
 });
-
-
