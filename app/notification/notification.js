32054fa5e6b3c92ecb0f102a90cb8e180951a350
Rentian Dong <r2dong@yahoo.com>
2018-11-21 16:03:43 -0600

    using eval for search and email sending

--- app/notification/notification.js
+++ app/notification/notification.js
@@ -1,7 +1,13 @@
 const CronJob = require('cron').CronJob;
+// they are used in eval(), which is not properly recognized
+// noinspection JSUnusedLocalSymbols
+const search_tweets = require('../search');
+// noinspection JSUnusedLocalSymbols
+const send_email = require('./email');
 
 const INTERVALS = {
     secondly: '* * * * * *',
+    minutely: '1 * * * * *',
     hourly: '1 1 * * * *',
     daily: '1 1 1 * * *',
     weekly: '1 1 1 * * 1',
@@ -11,7 +17,16 @@ const INTERVALS = {
 function start_auto_notifications() {
     for (const interval in INTERVALS) {
         new CronJob(INTERVALS[interval], () => {
-            console.log('message');
+            eval(`
+                let arr = [];
+                search_tweets('trump', 10).subscribe({
+                    next: tweet => arr.push(tweet),
+                    error: err => console.log(err),
+                    complete: () => console.log('complete')
+                });
+                send_email('r2dong@yahoo.com,', 'using eval to search and send mail', 'it works!');
+            `);
+            console.log('after eval');
         }, null, true, 'America/Chicago').start()
     }
 }
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
-const CronJob = require('cron').CronJob;
-const send_mail = require('./app/notification/email.js');
+const start_auto_notifications = require('./app/notification/notification');
 
 app.use(
 	bodyParser.urlencoded({extended: true}),
@@ -16,10 +15,7 @@ app.listen(port, () => {
     console.log('We are live on ' + port);
 	console.log('Environment: ' + app.settings.env);
 
-
-	new CronJob('0 * * * * *', () => {
-  		console.log('message');
-	}, null, true, 'America/Chicago').start();
-
-	send_mail('r2dong@yahoo.com', 'ah, title', 'ah, contents');
+	start_auto_notifications();
 });
+
+module.exports = app;
